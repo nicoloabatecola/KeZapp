7fd866aeff17608fc7ff3098f36c4def4073375a
nicoloabatecola <78588208+nicoloabatecola@users.noreply.github.com>
2021-03-01 15:35:46 +0100

    modifiche controller

--- kezapp-server-his/src/main/java/it/sirfin/kezappserverhis/controller/KeZappController.java
+++ kezapp-server-his/src/main/java/it/sirfin/kezappserverhis/controller/KeZappController.java
@@ -1,5 +1,14 @@
 package it.sirfin.kezappserverhis.controller;
 
+import it.sirfin.kezappserverhis.service1.KezappService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
 public class KeZappController {
     
+    @Autowired
+    KezappService kezappservice;
 }
