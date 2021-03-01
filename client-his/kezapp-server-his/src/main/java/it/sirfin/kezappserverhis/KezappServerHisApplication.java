eafffccc2333b068acefe92f692461775954279c
nicoloabatecola <78588208+nicoloabatecola@users.noreply.github.com>
2021-03-01 11:50:30 +0100

    creazione server

---
+++ client-his/kezapp-server-his/src/main/java/it/sirfin/kezappserverhis/KezappServerHisApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.kezappserverhis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KezappServerHisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KezappServerHisApplication.class, args);
+	}
+
+}
