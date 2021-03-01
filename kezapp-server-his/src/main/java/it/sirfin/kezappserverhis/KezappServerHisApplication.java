f78a00a68399fc0f05e68feffdefb6b402a35c44
nicoloabatecola <78588208+nicoloabatecola@users.noreply.github.com>
2021-03-01 17:18:46 +0100

    Main class pronta

--- kezapp-server-his/src/main/java/it/sirfin/kezappserverhis/KezappServerHisApplication.java
+++ kezapp-server-his/src/main/java/it/sirfin/kezappserverhis/KezappServerHisApplication.java
@@ -2,12 +2,18 @@ package it.sirfin.kezappserverhis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"it.sirfin.kezappserverhis"})
+@EnableJpaRepositories(basePackages = {"it.sirfin.kezappserverhis.repository"})
+@EntityScan(basePackages = {"it.sirfin.kezappserverhis.model"})
 public class KezappServerHisApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KezappServerHisApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KezappServerHisApplication.class, args);
+    }
 
 }
