5d566d161cd950b46ca493e24f2e7e7e0fa7bf51
nicoloabatecola <78588208+nicoloabatecola@users.noreply.github.com>
2021-03-01 15:52:08 +0100

    prova controller da rivedere

--- kezapp-server-his/src/main/java/it/sirfin/kezappserverhis/controller/KeZappController.java
+++ kezapp-server-his/src/main/java/it/sirfin/kezappserverhis/controller/KeZappController.java
@@ -1,14 +1,45 @@
 package it.sirfin.kezappserverhis.controller;
 
+import it.sirfin.kezappserverhis.dto.InviaMessaggioDto;
+import it.sirfin.kezappserverhis.dto.RichiediMessaggiDto;
+import it.sirfin.kezappserverhis.dto.RichiediRegistrazioneDto;
+import it.sirfin.kezappserverhis.model.Messaggio;
 import it.sirfin.kezappserverhis.service1.KezappService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin("*")
 public class KeZappController {
-    
+
     @Autowired
     KezappService kezappservice;
+
+    @RequestMapping("/registrazione")
+    @ResponseBody
+    public RichiediRegistrazioneDto registrazione() {
+        return new RichiediRegistrazioneDto(kezappservice.registrazione());
+    }
+
+    @RequestMapping("/inviatutti")
+    @ResponseBody
+    public RichiediMessaggiDto inviaTutti() {
+        return new RichiediMessaggiDto(kezappservice.inviaTutti(Messaggio));
+    }
+
+    @RequestMapping("/inviauno")
+    @ResponseBody
+    public InviaMessaggioDto inviaUno() {
+        return new InviaMessaggioDto(kezappservice.inviaUno(Messaggio));
+    }
+
+    @RequestMapping("/aggiorna")
+    @ResponseBody
+    public RichiediMessaggiDto aggiorna() {
+        return new RichiediMessaggiDto(kezappservice.aggiorna(Chat));
+    }
 }
